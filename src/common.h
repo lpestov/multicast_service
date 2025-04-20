9033c7fc4d7ba837611a9053019fea8518396dae
leo <Pestov.962@gmail.com>
2025-04-20 12:57:59 +0300

    #add server and client

--- src/common.h
+++ src/common.h
@@ -10,6 +10,9 @@ const unsigned short MULTICAST_PORT = 5007;
 const unsigned short SERVER_LISTEN_PORT = 5008;
 const std::string SERVER_PING_MESSAGE = "SERVER_DISCOVERY_PING";
 const std::string CLIENT_PONG_MESSAGE = "CLIENT_DISCOVERY_PONG";
+const int MULTICAST_TTL = 1;
+const std::chrono::seconds RESPONSE_TIMEOUT(5);
+const std::chrono::seconds PING_INTERVAL(10);
 
 void print_error(const std::string& msg);
 
