7acc1ccafb09fd3feae280d511f5c0a6133321f5
leo <Pestov.962@gmail.com>
2025-04-20 11:25:52 +0300

    Initial

---
+++ src/common.h
@@ -0,0 +1,19 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <string>
+#include <chrono>
+
+
+const std::string MULTICAST_GROUP = "224.1.1.1";
+const unsigned short MULTICAST_PORT = 5007;
+const unsigned short SERVER_LISTEN_PORT = 5008;
+const std::string SERVER_PING_MESSAGE = "SERVER_DISCOVERY_PING";
+const std::string CLIENT_PONG_MESSAGE = "CLIENT_DISCOVERY_PONG";
+
+void print_error(const std::string& msg);
+
+inline bool initialize_networking() { return true; }
+inline void cleanup_networking() {}
+
+#endif
